28a92d58492c5f0a374d433857f4ed21112c6375
mrDock21 <danes_warrior@hotmail.com>
2020-09-29 11:33:57 -0500

    Agregado area de cuadrado

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var input string
+	var side int = 1
+
+	// Read from console
+	_, err := fmt.Scan(&input)
+
+	if err == nil {
+		// Scan values from input string
+		integer, atoi_err := strconv.Atoi(input)
+		if atoi_err == nil {
+			side = integer
+			fmt.Printf("%d\n", side*side)
+		}
+	} else {
+		fmt.Println(err)
+	}
 }
